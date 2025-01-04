821444292c85cabfc58641feffb92a479619c2ef
willjasen <brandon@willjasen.com>
2025-01-03 19:43:03 -0500

    Update tracks-stat.sh

--- tracks-stat.sh
+++ tracks-stat.sh
@@ -6,3 +6,9 @@ apt-get update;
 # Install darkice and icecast2
 apt-get install darkice -y;
 apt-get install icecast2 -y;
+
+# Make darkice a service
+chmod +x chmod /home/willjasen/darkice.sh
+cp darkice.service /etc/systemd/system/darkice.service;
+systemctl daemon-reload;
+systemctl enable darkice.service;
