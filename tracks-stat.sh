263adaec1be13bc4366b28f8ad79bce08ef60548
willjasen <brandon@willjasen.com>
2025-01-03 19:27:30 -0500

    install dependencies

--- tracks-stat.sh
+++ tracks-stat.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
+# Install dependencies
+apt-get update;
+apt install git -y;
+
 # Install darkice and icecast2
-sudo apt-get update;
-sudo apt-get upgrade -y;
-sudo apt-get install darkice icecast2 -y;
+apt-get install darkice icecast2 -y;
