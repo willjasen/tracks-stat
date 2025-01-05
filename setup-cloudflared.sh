bdd3e144d5a37dbcadc5ad75c5adca7b2db686df
willjasen <brandon@willjasen.com>
2025-01-05 00:26:11 -0500

    stash

--- setup-cloudflared.sh
+++ setup-cloudflared.sh
@@ -34,6 +34,7 @@ mkdir -p /etc/cloudflared || error_exit "Failed to create cloudflared configurat
 systemctl stop cloudflared;
 rm /etc/systemd/system/cloudflared.service;
 systemctl daemon-reload;
+cloudflared service uninstall;
 
 # Create a systemd service file for cloudflared
 cat <<EOF > /etc/systemd/system/cloudflared.service
@@ -53,7 +54,7 @@ EOF
 
 # Change the config file
 ln -s /opt/tracks-stat/cloudflare-config.yml /etc/cloudflared/config.yml || error_exit "Failed to create config symlink";
-sed -i "s/your_actual_tunnel_id/${VAR1}/" /opt/tracks-stat/cloudflare-config.yml;
+sed -i "s/your_actual_tunnel_id/$VAR1/" /opt/tracks-stat/cloudflare-config.yml;
 
 # Reload systemd, enable and start the cloudflared service
 systemctl daemon-reload || error_exit "Failed to reload systemd daemon"
