add7f06a2371c20689a30fa93d9e7a883ae49174
willjasen <brandon@willjasen.com>
2025-01-05 06:20:57 -0500

    make duration min/max into vars

--- ffmpeg-test.sh
+++ ffmpeg-test.sh
@@ -2,6 +2,8 @@
 
 STREAM_HOST="http://rp.risk-mermaid.ts.net:8000/"
 CLIENT_INSTANCES=3;
+DURATION_MIN=5;
+DURATION_MAX=15;
 
 # Install dependencies
 # brew install coreutils;  # install gdate
@@ -13,7 +15,7 @@ NC='\033[0m' # No Color
 echo -e "${GREEN}Starting ${CLIENT_INSTANCES} streaming instances...${NC}";
 for i in $(seq 1 $CLIENT_INSTANCES); do
     SEED=$(gdate +%N)
-    DURATION=$(awk -v min=5 -v max=15 -v seed=$SEED 'BEGIN{srand(seed + PROCINFO["pid"]); print int(min+rand()*(max-min+1))}')
+    DURATION=$(awk -v min=$DURATION_MIN -v max=$DURATION_MAX -v seed=$SEED 'BEGIN{srand(seed + PROCINFO["pid"]); print int(min+rand()*(max-min+1))}')
     ffmpeg -i $STREAM_HOST -t $DURATION -progress pipe:1 -loglevel debug -f null - 2>&1 | grep --line-buffered "Statistics: .* bytes read" | sed 's/.*AVIOContext @ [^ ]* //' | awk -v duration=$DURATION -v yellow=$YELLOW -v nc=$NC '{print yellow "Duration: " duration " seconds, " $0 nc}' &
 done
 wait
