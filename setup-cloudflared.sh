0fece63fee65a7e134063b515f728bfaefe396d4
willjasen <brandon@willjasen.com>
2025-01-05 00:58:32 -0500

    remove comments

--- setup-cloudflared.sh
+++ setup-cloudflared.sh
@@ -30,21 +30,6 @@ fi
 # Create a configuration directory for cloudflared
 mkdir -p /etc/cloudflared || error_exit "Failed to create cloudflared configuration directory"
 
-
-
-
-# Check if config.yml symlink already points to our config
-#if [ "$(readlink /etc/cloudflared/config.yml)" != "/opt/tracks-stat/cloudflare-config.yml" ]; then
-    # Remove existing config if present
-#    if [ -f /etc/cloudflared/config.yml ]; then
-#        rm /etc/cloudflared/config.yml || error_exit "Failed to remove existing config";
-#    fi
-    # Create symlink
-#    ln -s /opt/tracks-stat/cloudflare-config.yml /etc/cloudflared/config.yml || error_exit "Failed to create config symlink";
-#fi
-
-
-# Change the config file
 # Verify VAR1 is set
 if [ -z "${VAR1}" ]; then
     error_exit "VAR1 is not set. Please set the tunnel ID variable.";
@@ -52,18 +37,9 @@ else
     # Debug tunnel ID configuration
     echo "Current tunnel ID: ${VAR1}"
 fi
-# Update config file with tunnel ID
-#sed -i "s/^tunnel: .*$/tunnel: ${VAR1}/" /opt/tracks-stat/cloudflare-config.yml || error_exit "Failed to update tunnel ID";
-#sed -i "s/^credentials-file: \/etc\/cloudflared\/.*\.json$/credentials-file: \/etc\/cloudflared\/${VAR1}.json/" /opt/tracks-stat/cloudflare-config.yml || error_exit "Failed to update credentials file path";
 
-# Reload systemd, enable and start the cloudflared service
-#systemctl daemon-reload || error_exit "Failed to reload systemd daemon"
-#systemctl enable cloudflared || error_exit "Failed to enable cloudflared service"
-#systemctl start cloudflared || error_exit "Failed to start cloudflared service"
-
-#systemctl stop cloudflared;
+# Clear out the service and redo it
 rm /etc/systemd/system/cloudflared.service;
-# Create a systemd service file for cloudflared
 cat <<EOF > /etc/systemd/system/cloudflared.service
 [Unit]
 Description=Cloudflare Tunnel
@@ -83,9 +59,3 @@ cloudflared service uninstall;
 cloudflared service install $VAR1;
 
 echo "Cloudflare Tunnel installation completed successfully"
-
-# Clear the cloudflard service
-    #systemctl stop cloudflared;
-    #rm /etc/systemd/system/cloudflared.service;
-    #systemctl daemon-reload;
-    #cloudflared service uninstall;
